589840e53339ffc93220b7724bd1db6b61fe46f6
Sowad Hossain <sowadhossain017@gmail.com>
2022-09-28 06:45:55 +0600

    takes string input

--- week1/hello.c
+++ week1/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-    char name[30] = "sowad hossain";
+    char name[30];
+    
+    printf("whats yr name:");
+
+    scanf("%s",&name);
 
     printf("hello world %s",name);
 }
